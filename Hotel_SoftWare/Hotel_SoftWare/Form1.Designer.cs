b138b8ceeaebeb9d9afaf349ff0274230e5b4116
ĐạtĐQ <dat1999vn@gmail.com>
2020-04-21 07:20:46 -0700

    sơn sửa
    thêm form

--- Hotel_SoftWare/Hotel_SoftWare/Form1.Designer.cs
+++ Hotel_SoftWare/Hotel_SoftWare/Form1.Designer.cs
@@ -28,13 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(52, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 94);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(359, 306);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
